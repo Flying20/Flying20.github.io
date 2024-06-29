a2fe19e3e920840e8071f8e5d6a183c4c2b17875
Flying20 <51212752+Flying20@users.noreply.github.com>
2024-06-28 22:43:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-setInterval(showTime, 1000);
-
-function showTime() {
-  document.getElementById("time").innerHTML=Date();
+// Toggles between light and dark mode
+function toggleDarkMode(e) {
+  if (e.checked) {
+    document.documentElement.style.setProperty("color-scheme", "dark");
+    document.getElementById("darkmode2").innerHTML = "&#x2600&#xFE0F";
+  }
+  else {
+    document.documentElement.style.setProperty("color-scheme", "light");
+    document.getElementById("darkmode2").innerHTML = "&#x1F319";
+  }
 }
 
-function showText() {
-  document.getElementById("text").innerHTML+=document.getElementById("textbox").value+'\n';
-  document.getElementById("textbox").value="";
+// Initialize dark mode toggle
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.getElementById("darkmode").checked = true;
+  document.getElementById("darkmode2").innerHTML = "&#x2600&#xFE0F";
 }
+
+// Update the time every second
+function updateTime() {
+  postMessage(Date());
+  setTimeout(updateTime, 1000);
+}
+
+/*
+Chrome and Firefox does not allow running web workers from a local file.
+The following workaround is found at https://stackoverflow.com/a/33432215/607407
+*/
+
+var worker = new Worker(URL.createObjectURL(new Blob(["("+updateTime.toString()+")()"], {type: "text/javascript"})));
+worker.onmessage = function(event) {
+  document.getElementById("time").innerHTML = event.data;
+};
